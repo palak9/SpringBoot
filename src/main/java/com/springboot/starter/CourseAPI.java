d33dcc60512fb71a0acc1ebf89ee467367ceefb8
Palak Rathi <palakrathi09@gmail.com>
2022-10-09 14:21:12 +0530

    Repo Structure

---
+++ src/main/java/com/springboot/starter/CourseAPI.java
@@ -0,0 +1,15 @@
+package com.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPI {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CourseAPI.class, args);
+
+	}
+
+}
